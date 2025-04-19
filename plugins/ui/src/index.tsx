4be4ea103d737cca9746bd0b3cd294edc1275103
Inrixia <Inrixia@gmail.com>
2025-04-19 18:34:54 +1200

    Enable page theme for ui settings

--- plugins/ui/src/index.tsx
+++ plugins/ui/src/index.tsx
@@ -12,14 +12,14 @@ import { lunaMuiTheme } from "./lunaTheme";
 export const unloads = new Set<LunaUnload>();
 
 const settingsPage = Page.register("LunaSettings", unloads);
-// // thx @n1ckoates re CoverTheme <3
-// settingsPage.pageStyles.background = `
-// radial-gradient(ellipse at top left, rgba(88, 10, 82, 0.5), transparent 70%),
-// radial-gradient(ellipse at center left, rgba(18, 234, 246, 0.5), transparent 70%),
-// radial-gradient(ellipse at bottom left, rgba(205, 172, 191, 0.5), transparent 70%),
-// radial-gradient(ellipse at top right, rgba(139, 203, 235, 0.5), transparent 70%),
-// radial-gradient(ellipse at center right, rgba(98, 103, 145, 0.5), transparent 70%),
-// radial-gradient(ellipse at bottom right, rgba(47, 48, 78, 0.5), transparent 70%)`;
+// thx @n1ckoates re CoverTheme <3
+settingsPage.pageStyles.background = `
+radial-gradient(ellipse at top left, rgba(88, 10, 82, 0.5), transparent 70%),
+radial-gradient(ellipse at center left, rgba(18, 234, 246, 0.5), transparent 70%),
+radial-gradient(ellipse at bottom left, rgba(205, 172, 191, 0.5), transparent 70%),
+radial-gradient(ellipse at top right, rgba(139, 203, 235, 0.5), transparent 70%),
+radial-gradient(ellipse at center right, rgba(98, 103, 145, 0.5), transparent 70%),
+radial-gradient(ellipse at bottom right, rgba(47, 48, 78, 0.5), transparent 70%)`;
 
 settingsPage.render(<ThemeProvider theme={lunaMuiTheme} children={<LunaSettings />} />);
 
