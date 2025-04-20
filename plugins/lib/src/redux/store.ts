c4748bae5ca87a0a67c692d095e496023a3e38d8
Inrixia <Inrixia@gmail.com>
2025-04-20 14:23:13 +1200

    Fix lib redux.store export types

--- plugins/lib/src/redux/store.ts
+++ plugins/lib/src/redux/store.ts
@@ -2,4 +2,4 @@ import { findModuleByProperty } from "@luna/core";
 
 import type { Store } from "redux";
 
-export const store = findModuleByProperty<Store>("replaceReducer", "function");
+export const store: Store = <Store>findModuleByProperty("replaceReducer", "function");
