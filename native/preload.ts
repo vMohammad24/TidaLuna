46ebfbdff3d82c2ddd08bf888a14c3315fc421ed
Inrixia <Inrixia@gmail.com>
2025-04-21 00:52:10 +1200

    Add error display to loading page

--- native/preload.ts
+++ native/preload.ts
@@ -13,11 +13,25 @@ ipcRenderer.invoke("__Luna.originalPreload").then(require);
 	await webFrame
 		.executeJavaScript(
 			`(async () => { 
-				const renderJs = await lunaNative.invoke("__Luna.renderJs");
-				const renderUrl = URL.createObjectURL(new Blob([renderJs], { type: "text/javascript" }));
-				window.luna ??= {};
-				window.luna.core = await import(renderUrl);
-				window.luna.core.modules["@luna/core"] = window.luna.core;
+				try {
+					const renderJs = await lunaNative.invoke("__Luna.renderJs");
+					const renderUrl = URL.createObjectURL(new Blob([renderJs], { type: "text/javascript" }));
+					window.luna ??= {};
+					window.luna.core = await import(renderUrl);
+					window.luna.core.modules["@luna/core"] = window.luna.core;
+				} catch (err) {
+					setTimeout(() => {
+						// Created in native/injector.ts
+						const messageContainer = document.getElementById('tidaluna-loading-text');
+						if (messageContainer !== null) {
+							messageContainer.innerHTML += \`
+							<h2>Failed to load luna.js</h2><br/>
+							<span style="color: red">\${err.message}</span><br/><br/>
+							<span>\${err.stack}</span>\`;
+						}
+					});
+					throw err;
+				}
 			})()`,
 			true,
 		)
