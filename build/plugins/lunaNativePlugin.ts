053257ce51fdf1273b4782bc0ff4fb16b3c0bd8e
Inrixia <Inrixia@gmail.com>
2025-04-30 02:14:48 +1200

    Fix native modules not supporting require (while keeping esm support)

--- build/plugins/lunaNativePlugin.ts
+++ build/plugins/lunaNativePlugin.ts
@@ -1,9 +1,15 @@
 import { build, Plugin } from "esbuild";
+import { resolve } from "path";
+import { pathToFileURL } from "url";
 import { defaultBuildOptions, TidalNodeVersion } from "../index.js";
 import { fileUrlPlugin } from "./fileUrl.js";
 import { buildCache } from "./outputCache.js";
 
 const buildOutput = buildCache(async (args) => {
+	// Resolve the absolute path and convert it to a file URL
+	const entryPath = resolve(args.path);
+	const entryUrl = pathToFileURL(entryPath).toString();
+
 	const { outputFiles, metafile } = await build({
 		...defaultBuildOptions,
 		entryPoints: [args.path],
@@ -15,6 +21,10 @@ const buildOutput = buildCache(async (args) => {
 		format: "esm",
 		external: ["@luna/*", "electron"],
 		plugins: [fileUrlPlugin],
+		banner: {
+			// Support require for native modules
+			js: `import { createRequire } from 'module';const require = createRequire(${JSON.stringify(entryUrl)})`,
+		},
 	});
 
 	const output = Object.values(metafile!.outputs)[0];
