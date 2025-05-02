4b163e5c27a36f7fc5df323263fde36f9039c938
Inrixia <Inrixia@gmail.com>
2025-05-02 14:35:16 +1200

    Fix LunaSetting flex

--- plugins/ui/src/components/settings/LunaSetting.tsx
+++ plugins/ui/src/components/settings/LunaSetting.tsx
@@ -8,6 +8,6 @@ export type LunaSettingProps = StackProps & LunaTitleValues;
 export const LunaSetting = React.memo((props: LunaSettingProps) => (
 	<Stack direction="row" {...props} title={undefined}>
 		<LunaTitle sx={{ flexShrink: 0 }} variant="h8" title={props.title} desc={props.desc} />
-		<Box sx={{ flexGrow: 1 }} children={props.children} />
+		<Box display="flex" sx={{ flexGrow: 1 }} children={props.children} />
 	</Stack>
 ));
