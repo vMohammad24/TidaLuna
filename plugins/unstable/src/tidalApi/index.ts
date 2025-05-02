7a9ab70560513f4e63926e15fbefe4f9c0513780
Inrixia <Inrixia@gmail.com>
2025-05-03 03:03:52 +1200

    memoize tidalApi

--- plugins/unstable/src/tidalApi/index.ts
+++ plugins/unstable/src/tidalApi/index.ts
@@ -1,7 +1,7 @@
 import { ftch, getCredentials, type Tracer } from "@luna/core";
 import { redux, type ItemId, type TAlbum, type TArtist, type TLyrics, type TTrackItem } from "@luna/lib";
 
-import type { VoidLike } from "@inrixia/helpers";
+import { Memo, memoize, type Memoized, type VoidLike } from "@inrixia/helpers";
 
 import { uTrace } from "../window.unstable";
 import type { TApiTrack, TApiTracks } from "./types/Tracks";
@@ -10,6 +10,7 @@ export * from "./types";
 
 export class TidalApi {
 	public static trace: Tracer = uTrace.withSource("TidalApi").trace;
+
 	public static async getAuthHeaders() {
 		const { clientId, token } = await getCredentials();
 		return {
@@ -17,44 +18,38 @@ export class TidalApi {
 			"x-tidal-token": clientId,
 		};
 	}
-
-	public static getQueryArgs() {
+	public static readonly queryArgs: Memoized<() => string> = Memo.argless(() => {
 		const store = redux.store.getState();
 		return `countryCode=${store.session.countryCode}&deviceType=DESKTOP&locale=${store.settings.language}`;
-	}
-	public static async track(trackId: ItemId) {
-		return ftch.json<TTrackItem>(`https://desktop.tidal.com/v1/tracks/${trackId}?${this.getQueryArgs()}`, {
+	});
+	public static fetch = memoize(async <T>(url: string): Promise<T> => {
+		return ftch.json<T>(url, {
 			headers: await this.getAuthHeaders(),
 		});
+	});
+
+	public static async track(trackId: ItemId) {
+		return this.fetch<TTrackItem>(`https://desktop.tidal.com/v1/tracks/${trackId}?${this.queryArgs()}`);
 	}
 	public static async lyrics(trackId: ItemId) {
-		return ftch.json<TLyrics>(`https://desktop.tidal.com/v1/tracks/${trackId}/lyrics?${this.getQueryArgs()}`, {
-			headers: await this.getAuthHeaders(),
-		});
+		return this.fetch<TLyrics>(`https://desktop.tidal.com/v1/tracks/${trackId}/lyrics?${this.queryArgs()}`);
 	}
 	public static async album(albumId: ItemId) {
-		return ftch.json<TAlbum>(`https://desktop.tidal.com/v1/albums/${albumId}?${this.getQueryArgs()}`, {
-			headers: await this.getAuthHeaders(),
-		});
+		return this.fetch<TAlbum>(`https://desktop.tidal.com/v1/albums/${albumId}?${this.queryArgs()}`);
 	}
 	public static async artist(artistId: ItemId) {
-		return ftch.json<TArtist>(`https://desktop.tidal.com/v1/artists/${artistId}?${this.getQueryArgs()}`, {
-			headers: await this.getAuthHeaders(),
-		});
+		return this.fetch<TArtist>(`https://desktop.tidal.com/v1/artists/${artistId}?${this.queryArgs()}`);
 	}
 
 	public static async *isrc(isrc: string): AsyncIterable<TApiTrack> {
-		const headers = await this.getAuthHeaders();
-		let resp = await ftch.json<TApiTracks | VoidLike>(`https://openapi.tidal.com/v2/tracks?${this.getQueryArgs()}&filter[isrc]=${isrc}`, {
-			headers,
-		});
+		let resp = await this.fetch<TApiTracks | VoidLike>(`https://openapi.tidal.com/v2/tracks?${this.queryArgs()}&filter[isrc]=${isrc}`);
 		while (true) {
 			if (resp?.data === undefined || resp.data.length === 0) break;
 			yield* resp.data;
 			if (resp.links.next === undefined) break;
-			resp = await ftch
-				.json<TApiTracks>(`https://openapi.tidal.com/v2/${resp.links.next}`, { headers })
-				.catch(this.trace.err.withContext(`Unexpected error when fetching Tidal ISRC ${isrc}`));
+			resp = await this.fetch<TApiTracks>(`https://openapi.tidal.com/v2/${resp.links.next}`).catch(
+				this.trace.err.withContext(`Unexpected error when fetching Tidal ISRC ${isrc}`),
+			);
 		}
 	}
 }
