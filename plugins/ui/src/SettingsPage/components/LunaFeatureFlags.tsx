ac5d68a6432ecb653ceecec2987074d5b8dd62e4
Inrixia <Inrixia@gmail.com>
2025-05-08 11:56:00 +1200

    Add hide button for FeatureFlags

--- plugins/ui/src/SettingsPage/components/LunaFeatureFlags.tsx
+++ plugins/ui/src/SettingsPage/components/LunaFeatureFlags.tsx
@@ -5,6 +5,10 @@ import { LunaSwitchSetting, LunaTitle, settingsSx } from "../../components";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 
+import SettingsIcon from "@mui/icons-material/Settings";
+import { grey } from "@mui/material/colors";
+import { SpinningButton } from "./SpinningButton";
+
 type FeatureFlag<K extends string = string> = {
 	created: number;
 	name: K;
@@ -19,32 +23,43 @@ const getFeatureFlags = (): FeatureFlags => redux.store.getState().featureFlags;
 
 export const LunaFeatureFlags = React.memo(() => {
 	const [featureFlags, setFeatureFlags] = React.useState(getFeatureFlags());
+	const [hide, setHidden] = React.useState(false);
+
 	const setFlag = React.useCallback((flag: FeatureFlag) => {
 		redux.actions["featureFlags/SET_FLAGS"]({ [flag.name]: { ...flag, value: !flag.value } });
 		setFeatureFlags(getFeatureFlags());
 	}, []);
 	return (
 		<Box>
-			<LunaTitle sx={{ marginBottom: 0 }} title="Feature flags" desc="Tidal desktop app feature flags" />
-			<Grid spacing={2} container sx={settingsSx}>
-				{Object.values(featureFlags.flags)
-					.sort((a, b) => a.name.localeCompare(b.name))
-					.map((flag) => (
-						<Grid
-							key={flag.name}
-							size={6}
-							sx={{ borderRadius: 4, backgroundColor: "rgba(0, 0, 0, 0.20)", boxShadow: 2, paddingLeft: 1.5 }}
-							children={
-								<LunaSwitchSetting
-									disabled={flag.type !== "BOOLEAN"}
-									title={flag.name[0].toUpperCase() + flag.name.slice(1).replaceAll("-", " ")}
-									onClick={setFlag.bind(null, flag)}
-									checked={flag.value}
-								/>
-							}
-						/>
-					))}
-			</Grid>
+			<LunaTitle
+				sx={{ marginBottom: 0 }}
+				title="Feature flags"
+				desc="Feature flags & experiments currently in the Tidal desktop app. These are internal from Tidal and not Luna features"
+				children={
+					<SpinningButton title={hide ? "Show flags" : "Hide flags"} onClick={() => setHidden(!hide)} icon={SettingsIcon} sxColor={grey.A400} />
+				}
+			/>
+			{!hide && (
+				<Grid spacing={2} container sx={settingsSx}>
+					{Object.values(featureFlags.flags)
+						.sort((a, b) => a.name.localeCompare(b.name))
+						.map((flag) => (
+							<Grid
+								key={flag.name}
+								size={6}
+								sx={{ borderRadius: 4, backgroundColor: "rgba(0, 0, 0, 0.20)", boxShadow: 2, paddingLeft: 1.5 }}
+								children={
+									<LunaSwitchSetting
+										disabled={flag.type !== "BOOLEAN"}
+										title={flag.name[0].toUpperCase() + flag.name.slice(1).replaceAll("-", " ")}
+										onClick={setFlag.bind(null, flag)}
+										checked={flag.value}
+									/>
+								}
+							/>
+						))}
+				</Grid>
+			)}
 		</Box>
 	);
 });
--- plugins/ui/src/components/LunaTitle.tsx
+++ plugins/ui/src/components/LunaTitle.tsx
@@ -12,7 +12,12 @@ export const LunaTitle = React.memo((props: LunaTitleProps) => {
 	props.variant ??= "h6";
 	return (
 		<Box>
-			<Typography {...props}>{props.title}</Typography>
+			<Typography {...props}>
+				<Box component="span" sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+					{props.title}
+					{props.children}
+				</Box>
+			</Typography>
 			{props.desc && <Typography variant="subtitle2" gutterBottom children={props.desc} />}
 		</Box>
 	);
