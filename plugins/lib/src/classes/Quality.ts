92b056a042788689e47347e430d3ead6d2447ea8
Inrixia <Inrixia@gmail.com>
2025-05-18 13:20:45 +1200

    Fix Quality lookups & ordering

--- plugins/lib/src/classes/Quality.ts
+++ plugins/lib/src/classes/Quality.ts
@@ -9,40 +9,50 @@ export class Quality {
 	) {
 		Quality.idxLookup[idx] = this;
 	}
-	public static readonly Lowest = new Quality(0, "Lowest", "#b9b9b9");
-	public static readonly Low = new Quality(1, "Low", "#b9b9b9");
-	public static readonly High = new Quality(2, "High", "#33FFEE");
-	public static readonly Atmos = new Quality(3, "Atmos", "#6ab5ff");
-	public static readonly Sony630 = new Quality(4, "Sony630", "#6ab5ff");
-	public static readonly MQA = new Quality(5, "MQA", "#F9BA7A");
-	public static readonly HiRes = new Quality(6, "HiRes", "#ffd432");
 
+	public static readonly HiRes = new Quality(6, "HiRes", "#ffd432");
+	public static readonly MQA = new Quality(5, "MQA", "#F9BA7A");
+	public static readonly Atmos = new Quality(4, "Atmos", "#6ab5ff");
+	public static readonly Sony630 = new Quality(3, "Sony630", "#6ab5ff");
+	public static readonly High = new Quality(2, "High", "#33FFEE");
+	public static readonly Low = new Quality(1, "Low", "#b9b9b9");
+	public static readonly Lowest = new Quality(0, "Lowest", "#b9b9b9");
+
+	/** The highest possible quality available */
 	public static readonly Max = Quality.HiRes;
 
 	public static readonly lookups = {
 		metadataTags: {
-			LOSSLESS: Quality.High,
-			[Quality.High.idx]: "LOSSLESS",
-			SONY_360RA: Quality.Sony630,
-			[Quality.Sony630.idx]: "SONY_360RA",
-			DOLBY_ATMOS: Quality.Atmos,
-			[Quality.Atmos.idx]: "DOLBY_ATMOS",
 			HIRES_LOSSLESS: Quality.HiRes,
 			[Quality.HiRes.idx]: "HIRES_LOSSLESS",
+
 			MQA: Quality.MQA,
 			[Quality.MQA.idx]: "MQA",
+
+			DOLBY_ATMOS: Quality.Atmos,
+			[Quality.Atmos.idx]: "DOLBY_ATMOS",
+
+			SONY_360RA: Quality.Sony630,
+			[Quality.Sony630.idx]: "SONY_360RA",
+
+			LOSSLESS: Quality.High,
+			[Quality.High.idx]: "LOSSLESS",
 		},
 		audioQuality: {
 			HI_RES_LOSSLESS: Quality.HiRes,
 			[Quality.HiRes.idx]: "HI_RES_LOSSLESS",
+
 			HI_RES: Quality.MQA,
 			[Quality.MQA.idx]: "HI_RES",
+
 			LOSSLESS: Quality.High,
 			[Quality.High.idx]: "LOSSLESS",
-			HIGH: Quality.High,
-			[Quality.High.idx]: "HIGH",
-			LOW: Quality.Low,
-			[Quality.Low.idx]: "LOW",
+
+			HIGH: Quality.Low,
+			[Quality.Low.idx]: "HIGH",
+
+			LOW: Quality.Lowest,
+			[Quality.Lowest.idx]: "LOW",
 		},
 	} as const;
 
