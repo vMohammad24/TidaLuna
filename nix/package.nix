06e74134e2cc64b4baa0e56113f78fb9f71d05e8
Inrixia <6373693+Inrixia@users.noreply.github.com>
2025-05-30 18:21:03 +0000

    Apply automatic changes

--- nix/package.nix
+++ nix/package.nix
@@ -2,12 +2,12 @@
 stdenv.mkDerivation (finalAttrs: rec {
   name = "TidaLuna";
   pname = "${name}";
-  version = "1.4.2-alpha";
+  version = "1.5.0-beta";
   src = fetchFromGitHub {
     owner = "Inrixia";
     repo = "${name}";
     rev = "${version}";
-    hash = "sha256-xwSbtCHfW6yQyMeFgRJSPQEi0kZJ3yqXVvJoBieG9uY=";
+    hash = "sha256-A6/TrRUyb/TPpacLpkDiFG7ymi1cQ1ryBAjQ0yAOUWM=";
   };
 
   nativeBuildInputs = [
