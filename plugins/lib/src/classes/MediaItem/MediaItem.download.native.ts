d57a86e1c1152bd119897d3510a090ce68783255
Inrixia <Inrixia@gmail.com>
2025-06-11 12:42:06 +1200

    Lock MediaItem.download to 1 at a time

--- plugins/lib/src/classes/MediaItem/MediaItem.download.native.ts
+++ plugins/lib/src/classes/MediaItem/MediaItem.download.native.ts
@@ -9,6 +9,7 @@ import { fetchMediaItemStream, type FetchProgress } from "@luna/lib.native";
 // Type import & native space so not a issue
 import type { redux } from "@luna/lib";
 
+import { Semaphore } from "@inrixia/helpers";
 import type { PlaybackInfo } from "../../helpers";
 import type { MetaTags } from "./MediaItem.tags";
 
@@ -23,28 +24,30 @@ const fileExists = async (path: string): Promise<boolean> => {
 
 const downloads: Record<redux.ItemId, { progress: FetchProgress; promise: Promise<void> } | undefined> = {};
 export const downloadProgress = async (trackId: redux.ItemId) => downloads[trackId]?.progress;
-export const download = async (playbackInfo: PlaybackInfo, path: string, tags?: MetaTags): Promise<void> => {
-	if (downloads[playbackInfo.trackId] !== undefined) return downloads[playbackInfo.trackId]!.promise;
-	try {
-		// Dont download if file already exists
-		if (await fileExists(path)) return;
-		const parsedPath = parse(path);
-		await mkdir(parsedPath.dir, { recursive: true });
-		const writeStream = createWriteStream(join(parsedPath.dir, sanitize(parsedPath.base)));
+const downloadSema = new Semaphore(1);
+export const download = async (playbackInfo: PlaybackInfo, path: string, tags?: MetaTags): Promise<void> =>
+	downloadSema.with(async () => {
+		if (downloads[playbackInfo.trackId] !== undefined) return downloads[playbackInfo.trackId]!.promise;
+		try {
+			// Dont download if file already exists
+			if (await fileExists(path)) return;
+			const parsedPath = parse(path);
+			await mkdir(parsedPath.dir, { recursive: true });
+			const writeStream = createWriteStream(join(parsedPath.dir, sanitize(parsedPath.base)));
 
-		const progress = { total: 0, downloaded: 0 };
-		const stream = await fetchMediaItemStream(playbackInfo, {
-			progress,
-			tags,
-		});
+			const progress = { total: 0, downloaded: 0 };
+			const stream = await fetchMediaItemStream(playbackInfo, {
+				progress,
+				tags,
+			});
 
-		const { resolve, reject, promise } = Promise.withResolvers<void>();
-		downloads[playbackInfo.trackId] = { progress, promise };
+			const { resolve, reject, promise } = Promise.withResolvers<void>();
+			downloads[playbackInfo.trackId] = { progress, promise };
 
-		stream.pipe(writeStream).on("finish", resolve).on("error", reject);
+			stream.pipe(writeStream).on("finish", resolve).on("error", reject);
 
-		await promise;
-	} finally {
-		delete downloads[playbackInfo.trackId];
-	}
-};
+			await promise;
+		} finally {
+			delete downloads[playbackInfo.trackId];
+		}
+	});
