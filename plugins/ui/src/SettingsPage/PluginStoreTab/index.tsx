e01a0855f2f0e7f076d7dda40d98bf2612e25fa4
Daniel Traynor <7traynor@gmail.com>
2025-07-10 21:22:16 -0400

    Add plugin store
    Add plugin store with 1 initial plugin for adding a track to multiple playlists

--- plugins/ui/src/SettingsPage/PluginStoreTab/index.tsx
+++ plugins/ui/src/SettingsPage/PluginStoreTab/index.tsx
@@ -29,6 +29,7 @@ addToStores("https://github.com/Aztup/luna-plugins/releases/download/latest/stor
 addToStores("https://github.com/vMohammad24/luna-plugins/releases/download/latest/store.json");
 addToStores("https://github.com/MathDesigns/luna-plugins/releases/download/latest/store.json");
 addToStores("https://github.com/otomir23/luna-plugins/releases/download/latest/store.json");
+addToStores("https://github.com/dantraynor/lunaplugins/releases/download/latest/store.json");
 
 export const PluginStoreTab = React.memo(() => {
 	const [_storeUrls, setPluginStores] = useState<string[]>(obyStore.unwrap(storeUrls));
