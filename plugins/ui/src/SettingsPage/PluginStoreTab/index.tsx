07e8c3ee61639581271336ce59d3c6e20ca6bfec
Kamil Pomykała <kamil@kamilpomykala.dev>
2025-07-31 22:48:10 +0200

    feat: Add @Akasiek/tidaluna-plugins store

--- plugins/ui/src/SettingsPage/PluginStoreTab/index.tsx
+++ plugins/ui/src/SettingsPage/PluginStoreTab/index.tsx
@@ -30,6 +30,7 @@ addToStores("https://github.com/vMohammad24/luna-plugins/releases/download/lates
 addToStores("https://github.com/MathDesigns/luna-plugins/releases/download/latest/store.json");
 addToStores("https://github.com/otomir23/luna-plugins/releases/download/latest/store.json");
 addToStores("https://github.com/dantraynor/lunaplugins/releases/download/latest/store.json");
+addToStores("https://github.com/Akasiek/tidaluna-plugins/releases/download/latest/store.json");
 
 export const PluginStoreTab = React.memo(() => {
 	const [_storeUrls, setPluginStores] = useState<string[]>(obyStore.unwrap(storeUrls));
