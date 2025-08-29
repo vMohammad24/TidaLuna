3120aab8644be16ce3ba7114765ac6291cc028e8
meowarex <info@atomix.one>
2025-08-29 13:29:44 +1000

    Fixed Tidal-HiFi Injection URI <3

--- native/injector.ts
+++ native/injector.ts
@@ -73,7 +73,7 @@ electron.app.whenReady().then(async () => {
 		}
 
 		// Bypass CSP & Mark meta scripts for quartz injection
-		if (req.url === "https://desktop.tidal.com/" || req.url === "https://listen.tidal.com/") {
+		if (req.url === "https://desktop.tidal.com/" || req.url === "https://tidal.com/" || req.url === "https://listen.tidal.com/") {
 			const res = await electron.net.fetch(req, { bypassCustomProtocolHandlers: true });
 			let body = await res.text();
 			body = body.replace(
