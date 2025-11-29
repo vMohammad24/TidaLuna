fd07a17af8420d66709c3019142620864c3fcc43
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2025-11-29 16:41:42 +0300

    yes

--- plugins/lib/src/helpers/getCredentials.ts
+++ plugins/lib/src/helpers/getCredentials.ts
@@ -10,8 +10,20 @@ type TidalCredentials = {
 	userId: string;
 };
 
+
+let credModule: (() => Promise<TidalCredentials>) | undefined;
 export const getCredentials = findModuleProperty<() => Promise<TidalCredentials>>(
 	(key, value) => key === "getCredentials" && typeof value === "function"
 )?.value || (async () => {
-	throw new Error("Tidal credentials module not found");
+	try {
+		if (!credModule) {
+			credModule = findModuleProperty<() => Promise<TidalCredentials>>(
+				(key, value) => key === "getCredentials" && typeof value === "function",
+			)?.value;
+			if (!credModule) throw new Error("Could not find Tidal credentials module");
+		}
+		return credModule();
+	} catch (err) {
+		throw new Error(`Failed to get Tidal credentials: ${(err as Error).message}`);
+	}
 });
